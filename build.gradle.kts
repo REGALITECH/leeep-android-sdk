4d3551b5496cbbd743ce31a2512d760d43507b58
sakmt <kirimotch@gmail.com>
2025-01-17 17:11:02 +0900

    no components

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ plugins {
 }
 
 group = "com.github.REGALITECH"
-version = "0.0.2"
+version = "0.0.3"
 
 publishing {
     publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["release"])
+        create<MavenPublication>("aarPublication") {
             artifactId = "leeep-android-sdk"
+            artifact(file("LeeepAndroidSdk.aar"))
             pom {
                 name.set("LeeepSDK")
                 description.set("LEEEP SDK for Android")
